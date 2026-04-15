fbe930cfdd24ef8c7c5dedc729223b36f35b33fa
skdev0619 <skdev0619@naver.com>
2026-04-15 11:37:22 +0900

    init commit

---
+++ miniorder/src/main/java/com/sparta/miniorder/MiniorderApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.miniorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniorderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniorderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sparta/miniorder/MiniorderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.miniorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniorderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
